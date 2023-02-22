2fb95a8079efebeed252ecdecd84781d5c501c75
lukaszrozylo <63679058+woschukasz68@users.noreply.github.com>
2023-02-22 10:08:03 +0100

    Update generate_subsystems_status.sh

--- generate_subsystems_status.sh
+++ generate_subsystems_status.sh
@@ -5,7 +5,7 @@ echo 'ENVIRONMENT,GRAFANA,GRAFANA,PROMETHEUS,PROMETHEUS,PROMETHEUS,KAFDROP,KAFDR
 echo '"",Exist,Status,Exist,Status,rate_of_up_jobs,Exist,Status,Exist,Status,Exist,Status,response_of_server,Exist,Status,Exist,Status,Exist,Status,Exist,Status' >> $output_file
 
 ./inventory_generator.sh
-ansible-playbook playbook_generating_of_subsystem_status.yml
+ansible-playbook playbook_generating_of_subsystem_status.yml --extra-vars "ansible_password=$password"
 sed -i '/^"/d' inventory
 
 sort -o subsystems_info.txt subsystems_info.txt
